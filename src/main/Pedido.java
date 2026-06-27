e19916868c7fd85850e9b5a3d7f12f57b156e171
alexisAbrahan <alexislautaroabrahan1@gmail.com>
2026-06-27 20:19:16 -0300

    wiiiiiiiiiiiii

--- src/main/Pedido.java
+++ src/main/Pedido.java
@@ -1,11 +1,58 @@
 package main;
 
-public interface Pedido {
+import java.util.ArrayList;
+import java.util.List;
 
-	float getPeso();
+public class Pedido {
+	
+	
+	float getPeso() {
+		return 0;
+	}
 
-	Direccion getDir();
+	Direccion getDir() {
+		return null;
+	}
 
-	float getPrecio();
+	float getPrecio() {
+		return 0;
+	}
+    public Estado estado;
+    private List<Item> productos;
 
+    public Pedido() {
+        this.productos = new ArrayList<>();
+        // ¡ESTA LÍNEA ES CLAVE! Si no la ponés acá, el método jamás se ejecuta
+        this.estadoInicial(); 
+    }
+
+    public void estadoInicial() {
+        this.setEstado(new Borrador(this));
+    }
+
+    public void agregarItem(Item item) {
+        this.productos.add(item);
+    }
+
+    public void quitarItem(Item item) {
+        this.productos.remove(item);
+    }
+
+    public void decrementerStock() {}
+    public void rembolsaCostoYEnvio() {}
+    public void rembolsaCosto() {}
+    public void rembolsaEnvio() {}
+    public void incrementarStock() {}
+
+    public List<Item> getProductos() {
+        return this.productos;
+    }
+
+    public Estado getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
 }
