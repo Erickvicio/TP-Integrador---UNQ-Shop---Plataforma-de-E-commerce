9a0a279e2497addc624f2f17cac29598c7e30d1d
Huezo,Lautaro <122928895+LHuezo@users.noreply.github.com>
2026-07-01 12:23:17 -0300

    Update Estado.java
    agrego los mensajes relacionados a las acciones que realizan los subsistemas

--- src/main/Estado.java
+++ src/main/Estado.java
@@ -15,4 +15,8 @@ public abstract class Estado {
 	
     public void agregarItem(Item item) {}
     public void quitarItem(Item item) {}
+
+	public void mandarMail(Subsistema s) {};	
+	public void mandarComprobante(Subsistema s) {};
+	public void mandarCupon(Subsistema s) {};
 }
