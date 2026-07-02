0eba6c5ee40064bc00f8494148c20d63879d74d0
Huezo,Lautaro <122928895+LHuezo@users.noreply.github.com>
2026-07-02 00:03:07 -0300

    Update Catalogo.java

--- src/main/Catalogo.java
+++ src/main/Catalogo.java
@@ -10,10 +10,10 @@ public class Catalogo {
     private String nombre;
     private int precio;
     private Categoria cat;
-    // En Java usamos List y ArrayList (con mayúsculas)
+
     private ArrayList<ItemDeCatalogo> item;
 
-    // Constructor para inicializar los atributos y la lista vacía
+    
     public Catalogo(String nombre, int precio, Categoria cat) {
         this.nombre = nombre;
         this.precio = precio;
@@ -21,9 +21,7 @@ public class Catalogo {
         this.item = new ArrayList<>(); 
     }
 
-    // ==========================================
-    // GETTERS & SETTERS
-    // ==========================================
+
 
     public String getNombre() {
         return nombre;
@@ -57,16 +55,7 @@ public class Catalogo {
         this.item.add(nuevoProducto);
     }
 
-    // ==========================================
-    // MÉTODOS ADICIONALES (Basados en tu boceto)
-    // ==========================================
-
-    /* Si querés que devuelva el nombre de la categoría directamente
-    public String getNombreCategoria() {
-        return (cat != null) ? cat.getNombre() : "Sin categoría";
-    }
-*/
-    // Podés cambiar la lógica de disponibilidad según los productos que tenga adentro
+    
     public boolean estaDisponible() {
         return !item.isEmpty();
     }
